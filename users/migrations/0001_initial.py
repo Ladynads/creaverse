fa83eb3cdcbfea1d3d43ae8816804ff756b902bc
Miss Nadia <gbqlaced@gmail.com>
2025-03-13 16:49:19 +0000

    Remove admin dependency from initial migration

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -12,7 +12,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
-        ('admin', '0001_initial')
     ]
 
     operations = [
