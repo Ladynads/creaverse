bb02029166827d08f725290d7c730ae07488d546
Miss Nadia <gbqlaced@gmail.com>
2025-03-14 13:55:30 +0000

    Enabled secure cookies for session and CSRF

--- creaverse/settings.py
+++ creaverse/settings.py
@@ -149,3 +149,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security settings
+SESSION_COOKIE_SECURE = True  # Ensures session cookies are only sent over HTTPS
+CSRF_COOKIE_SECURE = True  # Ensures CSRF cookie is only sent over HTTPS
